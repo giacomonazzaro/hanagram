232f19d6d710841cf9f7c8b388e75c6cfe3e296e
Giacomo Nazzaro <nazzarogiacomo@gmail.com>
2020-03-20 01:15:24 +0100

    refactoring

--- hanabi.py
+++ hanabi.py
@@ -5,18 +5,21 @@ from random import shuffle
         
 colors = ['white', 'red', 'blue', 'green', 'yellow']
 
-deck = [];        
-for color in colors:
-    for i in range(1, 6):
-        count = 2
-        if i == 1: count = 3
-        if i == 5: count = 1
-        for _ in range(count):
-            deck.append(namedtuple('Card', 'color value')(color, i))
+def new_deck():
+    deck = [];        
+    for color in colors:
+        for i in range(1, 6):
+            count = 2
+            if i == 1: count = 3
+            if i == 5: count = 1
+            for _ in range(count):
+                deck.append(namedtuple('Card', 'color value')(color, i))
 
-shuffle(deck)
+    shuffle(deck)
+    return deck
 
 def main():
+    deck = new_deck()
     for card in deck:
         print(str(card.value) + " " + card.color)
 
